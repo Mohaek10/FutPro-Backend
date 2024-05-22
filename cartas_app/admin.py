6955e73569b786c8388a976751ccc404b1b17b89
Mohaek10 <elkasmimoha@gmail.com>
2024-05-22 14:34:02 +0200

    Sobres app

--- cartas_app/admin.py
+++ cartas_app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from cartas_app.models import Jugador, Equipo, Comentario
+
 # Register your models here.
+admin.site.register(Jugador)
+admin.site.register(Equipo)
+admin.site.register(Comentario)
