2dc69051d83f92c59159f17dafb985a7ac41c3a2
Mohaek10 <elkasmimoha@gmail.com>
2024-05-22 15:07:05 +0200

    Added urls for login, register, logout

--- user_app/api/urls.py
+++ user_app/api/urls.py
@@ -1,7 +1,12 @@
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from django.urls import path
 
+from user_app.api.views import register, logout_view
+
 urlpatterns = [
+    # path('login/', obtain_auth_token, name='login'),
+    path('register/', register, name='register'),
+    path('logout/', logout_view, name='logout'),
     path('api/token', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
