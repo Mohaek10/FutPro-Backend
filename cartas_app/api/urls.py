0588dc7bc0e73ac34470624f1b3de95f5a88591d
Mohaek10 <elkasmimoha@gmail.com>
2024-05-22 13:38:17 +0200

    Primeros urls

--- cartas_app/api/urls.py
+++ cartas_app/api/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path, include
 
+from cartas_app.api.views import JugadorAV, JugadorDV
+
 urlpatterns = [
-    path('cartas/')
+    path('jugadores/', JugadorAV.as_view(), name='jugadores'),
+    path('jugadores/<int:pk>', JugadorDV.as_view(), name='jugador-detalle'),
 ]
