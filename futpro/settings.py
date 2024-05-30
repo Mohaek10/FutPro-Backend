9fdd9c43e511101ba21f5fbf7672a0ed46ab9106
Mohaek10 <elkasmimoha@gmail.com>
2024-05-30 10:47:39 +0200

    Migration Postgres succefull

--- futpro/settings.py
+++ futpro/settings.py
@@ -73,10 +73,21 @@ WSGI_APPLICATION = 'futpro.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'futpro',
+        'USER': 'moha',
+        'PASSWORD': 'admin',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
@@ -127,6 +138,10 @@ REST_FRAMEWORK = {
         'rest_framework.throttling.AnonRateThrottle',
         'rest_framework.throttling.UserRateThrottle',
     ],
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',
+    ),
+
     # 'DEFAULT_THROTTLE_RATES': {
     #     'anon': '10000/day',
     #     'user': '10000/day',
