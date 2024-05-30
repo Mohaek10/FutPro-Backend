ccc1bbba01a273e2502163e2d950200f37fac205
Mohaek10 <elkasmimoha@gmail.com>
2024-05-30 17:55:44 +0200

    Changes in model user

--- futpro/settings.py
+++ futpro/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'user_app',
     'rest_framework',
     'rest_framework_simplejwt',
+    'rest_framework_simplejwt.token_blacklist',
 ]
 
 MIDDLEWARE = [
@@ -154,10 +155,19 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    'ROTATE_REFRESH_TOKENS': True,  # Cada vez que se refresca el token, se crea uno nuevo
-    'ACCESS_TOKEN_LIFETIME': timedelta(hours=12),  # Ampliar a 12 horas, por ejemplo
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),  # Ampliar a 1 día, por ejemplo
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=30),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=1),
+    'ROTATE_REFRESH_TOKENS': False,
+    'BLACKLIST_AFTER_ROTATION': True,
+    'ALGORITHM': 'HS256',
+    'SIGNING_KEY': SECRET_KEY,
+    'VERIFYING_KEY': None,
+    'AUDIENCE': None,
+    'ISSUER': None,
     'AUTH_HEADER_TYPES': ('Bearer',),
-    'AUTH_HEADER_NAME': 'HTTP_AUTHORIZATION',
-
+    'USER_ID_FIELD': 'id',
+    'USER_ID_CLAIM': 'user_id',
+    'AUTH_TOKEN_CLASSES': ('rest_framework_simplejwt.tokens.AccessToken',),
+    'TOKEN_TYPE_CLAIM': 'token_type',
+    'JTI_CLAIM': 'jti',
 }
--- user_app/api/urls.py
+++ user_app/api/urls.py
@@ -1,10 +1,11 @@
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 from django.urls import path
 
-from user_app.api.views import register, logout_view
+from user_app.api.views import register, logout_view, login_view
 
 urlpatterns = [
     # path('login/', obtain_auth_token, name='login'),
+    path('login/', login_view, name='login'),
     path('register/', register, name='register'),
     path('logout/', logout_view, name='logout'),
     path('api/token', TokenObtainPairView.as_view(), name='token_obtain_pair'),
