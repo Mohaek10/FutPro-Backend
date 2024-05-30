d8d3bdb7d71a27d928c9cbd93350ac297922133d
Mohaek10 <elkasmimoha@gmail.com>
2024-05-30 17:55:44 +0200

    Changes in model user

--- user_app/api/views.py
+++ user_app/api/views.py
@@ -1,3 +1,4 @@
+from django.contrib import auth
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
@@ -8,16 +9,47 @@ from rest_framework_simplejwt.tokens import RefreshToken
 
 
 @api_view(['POST'])
+def login_view(request):
+    data = {}
+    if request.method == 'POST':
+        email = request.data.get('email')
+        password = request.data.get('password')
+
+        account = auth.authenticate(email=email, password=password)
+
+        if account is not None:
+            data['response'] = 'Login exitoso'
+            data['email'] = account.email
+            data['username'] = account.username
+            data['first_name'] = account.first_name
+            data['last_name'] = account.last_name
+            data['phone_number'] = account.phone_number
+            refresh = RefreshToken.for_user(account)
+            data['token'] = {
+                'refresh': str(refresh),
+                'access': str(refresh.access_token),
+            }
+            return Response(data, status=status.HTTP_200_OK)
+        else:
+            data['response'] = 'Error de autenticación'
+            return Response(data, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['POST'])
 def logout_view(request):
     # Invalidar el token, para que no pueda ser usado nuevamente
+    data = {}
     try:
         refresh_token = request.data['refresh']
         token = RefreshToken(refresh_token)
         token.blacklist()
-        return Response('Sesion cerrada', status=status.HTTP_200_OK)
+        data['response'] = 'Sesion cerrada'
+        return Response(data, status=status.HTTP_200_OK)
     except Exception as e:
         print(e)
-        return Response('Token invalido', status=status.HTTP_400_BAD_REQUEST)
+        data['response'] = ('Token invalido, o no se ha proporcionado el token de refresco, por favor inicie sesion '
+                            'nuevamente')
+        return Response(data, status=status.HTTP_400_BAD_REQUEST)
 
 
 @api_view(['POST'])
@@ -30,8 +62,9 @@ def register(request):
             data['response'] = 'El registro del usuario fue exitoso'
             data['username'] = account.username
             data['email'] = account.email
-            # token = Token.objects.get(user=account).key
-            # data['token'] = token
+            data['first_name'] = account.first_name
+            data['last_name'] = account.last_name
+            data['phone_number'] = account.phone_number
             refresh = RefreshToken.for_user(account)
             data['token'] = {
                 'refresh': str(refresh),
