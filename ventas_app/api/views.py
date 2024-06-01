788f0bc6063ba8f0292706b7a1ec37fdc5b7bd39
Mohaek10 <elkasmimoha@gmail.com>
2024-06-01 22:35:12 +0200

    Cambios de logica de venta

--- ventas_app/api/views.py
+++ ventas_app/api/views.py
@@ -1,36 +1,53 @@
+from django.shortcuts import get_object_or_404
 from rest_framework import status, generics
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.exceptions import ValidationError
+from rest_framework.views import APIView
 
 from cartas_app.models import Jugador, JugadorUsuario
 from ventas_app.models import Venta
-from ventas_app.api.serializers import CompraSerializer, VentaSerializer
+from ventas_app.api.serializers import CompraSerializer, VentaSerializer, MercadoSistemaSerializer, \
+    CompraSistemaSerializer
 
 
-class ComprarJugador(generics.CreateAPIView):
-    serializer_class = CompraSerializer
+class MercadoSistemaList(generics.ListAPIView):
+    serializer_class = MercadoSistemaSerializer
     permission_classes = [IsAuthenticated]
 
-    def perform_create(self, serializer):
-        jugador = serializer.validated_data.get('jugador')
-        cantidad = serializer.validated_data.get('cantidad')
-        usuario = self.request.user
-        costo_total = jugador.valor * cantidad
+    def get_queryset(self):
+        return Jugador.objects.filter(en_mercado=True, isActive=True)
 
-        if usuario.futcoins < costo_total:
-            raise ValidationError("No tienes suficientes futcoins para comprar este jugador.")
 
-        usuario.futcoins -= costo_total
-        usuario.save()
+class ComprarMercadoSistema(APIView):
+    permission_classes = [IsAuthenticated]
 
-        # Verificar si el jugador ya está en la colección del usuario
-        jugador_usuario, created = JugadorUsuario.objects.get_or_create(usuario=usuario, jugador=jugador)
-        if not created:
-            jugador_usuario.cantidad += cantidad
-            jugador_usuario.save()
-        else:
-            serializer.save(usuario=usuario, cantidad=cantidad)
+    def post(self, request):
+        serializer = CompraSistemaSerializer(data=request.data)
+        if serializer.is_valid():
+            jugador_id = serializer.validated_data.get('jugador_id')
+            cantidad = serializer.validated_data.get('cantidad')
+            usuario = request.user
+            jugador = get_object_or_404(Jugador, id=jugador_id, en_mercado_sistema=True, isActive=True)
+
+            costo_total = jugador.valor * cantidad
+            if usuario.futcoins < costo_total:
+                return Response({'error': 'No tienes suficientes futcoins para comprar este jugador.'},
+                                status=status.HTTP_400_BAD_REQUEST)
+
+            usuario.futcoins -= costo_total
+            usuario.save()
+
+            jugador_usuario, created = JugadorUsuario.objects.get_or_create(usuario=usuario, jugador=jugador)
+            if not created:
+                jugador_usuario.cantidad += cantidad
+                jugador_usuario.save()
+            else:
+                serializer.save(usuario=usuario, cantidad=cantidad)
+
+            return Response({'success': 'Jugador comprado exitosamente.'}, status=status.HTTP_200_OK)
+
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 class VenderJugador(generics.CreateAPIView):
