97ae1e243ac22cefbc630aa5a799860ab348effb
Mohaek10 <elkasmimoha@gmail.com>
2024-06-02 16:29:59 +0200

    Jugador Usuario API VIew

--- cartas_app/api/urls.py
+++ cartas_app/api/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
-from cartas_app.api.views import JugadorAV, JugadorDV, EquipoAV, ComentarioCreate, ComentarioList, ComentarioDetalle
+from cartas_app.api.views import JugadorAV, JugadorDV, EquipoAV, ComentarioCreate, ComentarioList, ComentarioDetalle, \
+    JugadorUsuarioView
 
 router = DefaultRouter()
 router.register('equipos', EquipoAV, basename='equipo')  # basename='equipo' es opcional
@@ -14,6 +15,8 @@ urlpatterns = [
     # Listar comentarios de un jugador apartir de su id
     path('jugador/<int:pk>/comentarios/', ComentarioList.as_view(), name='comentarios'),
     path('comentarioById/<int:pk>', ComentarioDetalle.as_view(), name='comentarioById'),
+
+    path('usuarios/<str:username>/jugadores/', JugadorUsuarioView.as_view(), name='jugador-usuario'),
 
     path('', include(router.urls)),
 ]
