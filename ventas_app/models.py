53ecd5fa8692b94ce39ff2d9be8e9dc3fe18a158
Mohaek10 <elkasmimoha@gmail.com>
2024-06-02 20:35:47 +0200

    Add transaccion models

--- ventas_app/models.py
+++ ventas_app/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from user_app.models import Account
-from cartas_app.models import JugadorUsuario
+from cartas_app.models import JugadorUsuario, Jugador
 
 
 class VentaUsuario(models.Model):
@@ -12,3 +12,14 @@ class VentaUsuario(models.Model):
 
     def __str__(self):
         return f"{self.jugador_usuario.jugador.nombreCompleto} en venta por {self.precio} FutCoins"
+
+
+class Transaccion(models.Model):
+    comprador = models.ForeignKey(Account, related_name='compras', on_delete=models.CASCADE)
+    vendedor = models.ForeignKey(Account, related_name='ventas', on_delete=models.CASCADE, null=True, blank=True)
+    jugador = models.ForeignKey(Jugador, on_delete=models.CASCADE)
+    precio = models.DecimalField(max_digits=10, decimal_places=2)
+    fecha = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.comprador.username} compró {self.jugador.nombreCompleto} por {self.precio} FutCoins"
