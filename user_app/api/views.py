fa4435f1727cd34c1cb5e548c8fdd5f6f4590fb8
Mohaek10 <elkasmimoha@gmail.com>
2024-06-05 14:10:07 +0200

    Add check-admin

--- user_app/api/views.py
+++ user_app/api/views.py
@@ -3,7 +3,7 @@ from rest_framework import serializers
 from django.contrib import auth
 from django.contrib.auth.password_validation import validate_password
 from rest_framework import status, generics, viewsets
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework.generics import get_object_or_404
 from rest_framework.permissions import IsAuthenticated, AllowAny, IsAdminUser
 from rest_framework.response import Response
@@ -98,6 +98,13 @@ def register(request):
             }
             return Response(data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def check_admin_status(request):
+    user = request.user
+    return Response({'is_admin': user.is_admin}, status=status.HTTP_200_OK)
 
 
 class UserProfileView(generics.RetrieveUpdateAPIView):
