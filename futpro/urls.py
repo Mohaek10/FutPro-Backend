24cbbdc3a522f28d0160483f5022aa7631a81f38
Mohaek10 <elkasmimoha@gmail.com>
2024-06-08 17:12:37 +0200

    Subir imagenes

--- futpro/urls.py
+++ futpro/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+
+from futpro import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,3 +27,6 @@ urlpatterns = [
     path('api/ventas/', include('ventas_app.api.urls')),
 
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
