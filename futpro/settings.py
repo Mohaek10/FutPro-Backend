10c8e0ec48ae9d1579a4c0a25e640b20293d26bc
Mohaek10 <elkasmimoha@gmail.com>
2024-06-09 18:18:32 +0200

    add database dump

--- futpro/settings.py
+++ futpro/settings.py
@@ -151,7 +151,7 @@ REST_FRAMEWORK = {
         'rest_framework.renderers.JSONRenderer',
     ),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
-    'PAGE_SIZE': 10,
+    'PAGE_SIZE': 12,
 
     # 'DEFAULT_THROTTLE_RATES': {
     #     'anon': '10000/day',
